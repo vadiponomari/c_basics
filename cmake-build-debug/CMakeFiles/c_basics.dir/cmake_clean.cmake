087b3ed814e942e134faf31d6de28f3abaeab038
Vadim Ponomarev <vadim.ponomarev@alibaba-inc.com>
2021-01-31 21:43:20 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/c_basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_basics.dir/main.cpp.o"
+  "c_basics"
+  "c_basics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
